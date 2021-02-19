a0037ed0d3bc423634820a6f35796a98cd2e154a
Matthew Rosell Felix <matthewgz241@gmail.com>
2021-02-18 21:23:44 -0500

    Fix

--- src/utils/getLatLng.js
+++ src/utils/getLatLng.js
@@ -1,6 +1,6 @@
 const getLatLng = (data) => ({
-  lat: data.geometry.coordinates[1],
-  lng: data.geometry.coordinates[0],
+  lat: data?.geometry?.coordinates[1],
+  lng: data?.geometry?.coordinates[0],
 })
 
 export default getLatLng
